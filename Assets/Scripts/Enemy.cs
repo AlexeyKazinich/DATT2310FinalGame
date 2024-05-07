dfd972821d2eaa8956018aaa06d832108116aed0
AlexeyKazinich <alexey.kazinich@gmail.com>
2024-05-06 20:04:53 -0400

    Added enemies
    - Created an Enemy prefab, and added enemies. They have health and a healthbar above them

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 10;
+    public float maxHealth = 10;
+
+    [SerializeField] FloatingHealthBar healthBar;
+
+    private void Awake()
+    {
+        healthBar = GetComponentInChildren<FloatingHealthBar>();
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        this.health -= damageAmount;
+        healthBar.UpdateHealthBar(health,maxHealth);
+        if(this.health < 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            TakeDamage(2);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        TakeDamage(1);
+    }
+}
