fbc5097a4c754a826dfa27e9bad494928d83c67c
AlexeyKazinich <alexey.kazinich@gmail.com>
2024-05-07 02:23:47 -0400

    Update PlayerInfo.cs

--- Assets/Scripts/Player/PlayerInfo.cs
+++ Assets/Scripts/Player/PlayerInfo.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class PlayerInfo : MonoBehaviour
 {
-    public static int CurrentHealth;
-    public static int MaxHealth;
+    //these are just defaults
+    public static int CurrentHealth = 15;
+    public static int MaxHealth = 15;
+    public static int CurrentXP = 0;
+    public static int MaxXP = 100;
+    public static int level = 1;
 
-    public static int CurrentXP;
-    public static int MaxXP;
-    public static int level;
+
+
+    public static void addXP(int xp)
+    {
+        CurrentXP += xp;
+        if(CurrentXP >= MaxXP)
+        {
+            CurrentXP -= MaxXP;
+            level++;
+        }
+    }
 }
