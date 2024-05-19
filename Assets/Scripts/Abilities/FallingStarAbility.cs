f87e1c3a9f0e292c0d7577d2b6ff0a585d5fd24e
AlexeyKazinich <alexey.kazinich@gmail.com>
2024-05-19 15:59:59 -0400

    FallingStars Update
    -Added logic to falling Stars

--- Assets/Scripts/Abilities/FallingStarAbility.cs
+++ Assets/Scripts/Abilities/FallingStarAbility.cs
@@ -6,8 +6,22 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New FallingStarAbility", menuName = "Abilities/FallingStar")]
 public class FallingStarAbility : Ability
 {
+    public GameObject aoePrefab;
+    public float radius = 5f;
+    public float duration = 3f;
      protected override void Activate()
     {
         Debug.Log("FALLING STAR USED!!!");
+        if (Camera.main == null) return;
+
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 10f; // Distance from the camera
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+
+
+        //Instantiate the AOE effect at the mouse position
+        GameObject aoeInstance = Instantiate(aoePrefab,worldPosition,Quaternion.identity);
+        aoeInstance.GetComponent<FallingStarEffect>().Initialize(duration, radius);
+
     }
 }
---
+++ Assets/Scripts/Abilities/FallingStarEffect.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingStarEffect : MonoBehaviour
+{
+    public float damage = 2f;
+    public float tickRate = 1f;
+    private float radius;
+    private string enemyTag = "Enemy";
+
+    private float nextTickTime;
+
+    public void Initialize(float duration, float radius)
+    {
+        this.radius = radius;
+        gameObject.transform.localScale.Set(radius, radius, 1);
+        nextTickTime = Time.time + tickRate;
+        Destroy(gameObject, duration);
+    }
+
+    private void Start()
+    {
+        gameObject.transform.localScale = new Vector3(10, 10, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextTickTime)
+        {
+            nextTickTime += tickRate;
+            TickDamage();
+        }
+    }
+
+    private void TickDamage()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position,radius);
+
+        foreach(var hitCollider in colliders)
+        {
+            if (hitCollider.CompareTag(enemyTag))
+            {
+                Enemy enemy = hitCollider.GetComponent<Enemy>();
+                if(enemy != null)
+                {
+                    enemy.TakeDamage(damage);
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,radius);
+    }
+}
